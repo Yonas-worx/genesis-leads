da8c2ec398a011c7d24f1ab7100feabafae46904
Marwan Bassam <marwanbassam13@gmail.com>
2025-07-19 16:58:49 +0400

    TEST 4.0 trying email contact

--- src/backend/http-functions.js
+++ src/backend/http-functions.js
@@ -347,7 +347,7 @@ export async function post_sendLead(request) {
         // Send Email to notify
         // Saleh Omnia - 4671a558-ea3a-4b6b-b5d7-df740f749221
         // Fathima Sayana - f0dd4eb3-3ce8-4faf-8269-4dd728d48bc5
-        // Marwan - 744d4e71-893c-4bc6-81c3-2ed14214c6be
+        // Marwan - 7456d013-de65-4e46-a73e-fbcbfeced4d3
         let Emailvariables = {
             Fullname: jsonBody["fullName"],
             Email: jsonBody["email"],
@@ -361,10 +361,10 @@ export async function post_sendLead(request) {
             console.log("Started email sending process");
             // triggeredEmails.emailContact("UrKPsGW", "4671a558-ea3a-4b6b-b5d7-df740f749221",{variables: Emailvariables})
             // triggeredEmails.emailContact("UrKPsGW", "f0dd4eb3-3ce8-4faf-8269-4dd728d48bc5",{variables: Emailvariables})
-            triggeredEmails.emailMember("UrKPsGW", "744d4e71-893c-4bc6-81c3-2ed14214c6be", { variables: Emailvariables })
+            triggeredEmails.emailContact("UrKPsGW", "7456d013-de65-4e46-a73e-fbcbfeced4d3", { variables: Emailvariables })
                 .then(() => console.log("Email Sent to Marwan"))
                 .catch(err => console.error("Error sending email:", err));
-            triggeredEmails.emailMember("UrKPsGW", "1e34797c-5aef-48e9-8545-7f20ce97ec5d", { variables: Emailvariables })
+            triggeredEmails.emailContact("UrKPsGW", "1e34797c-5aef-48e9-8545-7f20ce97ec5d", { variables: Emailvariables })
                 .then(() => console.log("Email Sent to Marwan personal"))
                 .catch(err => console.error("Error sending email:", err));
         }
